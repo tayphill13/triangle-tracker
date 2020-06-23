b85a01f4448406d68fb5824c6797976a6214d2dd
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-22 21:36:43 -0700

    some changes to package json and main.js, reinstalling some things for a build

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+const Triangle = require('./../src/triangle.js').triangleModule;
